9fa4547e93fb7cd1ff7103d15ddea841b5d50b19
Дмитрий <mohassel@yandex.ru>
2026-06-29 02:34:30 +0300

    Первый блог на Django

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+
+from django.views.generic import ListView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
